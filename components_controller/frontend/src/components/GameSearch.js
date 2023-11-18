d9adf664b7e874df77916bfce1ccd9c4a1f635b5
AdiRomanov <romanovadi44@gmail.com>
2023-11-18 15:36:01 +0200

    Added search engine v1

--- components_controller/frontend/src/components/GameSearch.js
+++ components_controller/frontend/src/components/GameSearch.js
@@ -1,5 +1,4 @@
-// src/components/GameSearch.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const GameSearch = () => {
   const [query, setQuery] = useState('');
@@ -7,7 +6,37 @@ const GameSearch = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [searched, setSearched] = useState(false);
+  const [matchingGame, setMatchingGame] = useState(null);
 
+  // Function to handle the API response
+  const handleApiResponse = async (response) => {
+    try {
+      if (!response.ok) {
+        throw new Error('Failed to fetch search results');
+      }
+
+      const data = await response.json();
+      console.log('API Response:', data);
+
+      return data;
+    } catch (error) {
+      setError('An error occurred while fetching search results.');
+      console.error('Error:', error);
+      throw error; // Re-throw the error to stop further processing
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Function to process the data from the API
+  const handleData = (data) => {
+    // Find the matching game by info.name
+    const matchingGame = data.find((game) => game.info.name.toLowerCase() === query.toLowerCase());
+    setResults(data);
+    setMatchingGame(matchingGame);
+  };
+
+  // Function to handle the search
   const handleSearch = async (e) => {
     e.preventDefault();
     setLoading(true);
@@ -20,19 +49,15 @@ const GameSearch = () => {
 
       // Perform a fetch or use your preferred method to send a request to the Django backend
       const response = await fetch(`http://127.0.0.1:8000/api/search-game/?q=${query}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch search results');
-      }
+      const data = await handleApiResponse(response);
 
-      const data = await response.json();
-      console.log('API Response:', data);
-      setResults(data.results);
+      handleData(data);
     } catch (error) {
-      setError('An error occurred while fetching search results.');
-    } finally {
-      setLoading(false);
+      // Error handling is already done in handleApiResponse
     }
   };
+
+
 
   return (
     <div>
@@ -54,28 +79,18 @@ const GameSearch = () => {
 
       {searched && (
         <div>
-          {results.length > 0 ? (
+          {matchingGame ? (
             <>
               <h2 style={{ color: 'white' }}>Search Results:</h2>
-              <div style={{ color: 'white' }}>
-                {/* Find the matching game by info.name */}
-                const matchingGame = results.find((game) => game.info.name === query);
-
-                {/* Display details for the matching game */}
-                {matchingGame ? (
-                  <>
-                    <p>Name: {matchingGame.info.name}</p>
-                    <p>Description: {matchingGame.info.description}</p>
-                    <p>Developer: {matchingGame.info.developer}</p>
-                    {/* Add other fields as needed */}
-                  </>
-                ) : (
-                  <p style={{ color: 'white' }}>No matching result found.</p>
-                )}
+              <div key={matchingGame.id} style={{ color: 'white' }}>
+                <p>Name: {matchingGame.info.name}</p>
+                <p>Description: {matchingGame.info.description}</p>
+                <p>Developer: {matchingGame.info.developer}</p>
+                {/* Add other fields as needed */}
               </div>
             </>
           ) : (
-            <p style={{ color: 'white' }}>No results found.</p>
+            <p style={{ color: 'white' }}>No matching result found.</p>
           )}
         </div>
       )}
