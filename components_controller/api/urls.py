cec267a96dd965bb34237ea5633b57f7c43ce12d
Adi <romanovadi44@gmail.com>
2024-01-19 17:43:17 +0200

    Update urls.py

--- components_controller/api/urls.py
+++ components_controller/api/urls.py
@@ -2,12 +2,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import GameViewSet, search_game_by_name_view
 from .views import SearchGameViewSet
+from .views import get_csrf_token
+from .views import receive_data
 
 router = DefaultRouter()
-router.register(r'games', GameViewSet)  # Setează ruta pentru resursa 'jocuri' utilizând GameViewSet
-router.register(r'search-game', SearchGameViewSet)  # Setează ruta pentru resursa 'cauta-joc' utilizând SearchGameViewSet
+router.register(r'games', GameViewSet)
+router.register(r'search-game', SearchGameViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('search-game-by-name/<str:name>/', search_game_by_name_view, name='search-game-by-name'),
+    path('get-csrf-token/', get_csrf_token, name='get_csrf_token'),
+    path('receive-data/', receive_data, name='receive_data'),
 ]
